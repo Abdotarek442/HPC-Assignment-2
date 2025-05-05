409ce6fcf1a2a14dbeb172cb9696afd2be2f7910
hagarabdelmotalb <114605750+hagarabdelmotalb@users.noreply.github.com>
2025-05-05 20:53:47 +0300

    Update main.c

--- problem1/main.c
+++ problem1/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <mpi.h>
 
-int isPrime(int n){
+int isPrime(int n) {
     if (n < 2) return 0;
-    for (int i = 2; i <= n / 2; i++) {
-        if (n % i == 0)
-            return 0;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return 0;
     }
     return 1;
 }
 
 int main(int argc, char** argv) {
-    int x, y, rank, size, local_count = 0, total_count = 0;
-    int start, end;
+    int x, y, rank, size;
+    int start, end, local_count = 0, total_count = 0;
+    int local_primes[100], idx = 0;
     double startTime, endTime;
 
     MPI_Init(&argc, &argv);
@@ -20,9 +20,9 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     if (rank == 0) {
-        printf("Enter lower bound: ");
+        printf("enter lower bound: ");
         scanf("%d", &x);
-        printf("Enter upper bound: ");
+        printf("enter upper bound: ");
         scanf("%d", &y);
     }
 
@@ -32,27 +32,68 @@ int main(int argc, char** argv) {
     MPI_Bcast(&y, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     int range = y - x + 1;
-    int base = range / size;
-    int remaining = range % size;
+    int base = range / (size - 1);
+    int remaining = range % (size - 1);
 
-    int extra = (rank < remaining) ? 1 : 0;
-    int offset = (rank < remaining) ? rank : remaining;
-    start = x + rank * base + offset;
+    int extra = (rank - 1 < remaining) ? 1 : 0;  
+    int offset = (rank - 1 < remaining) ? rank - 1 : remaining; 
+    start = x + (rank - 1) * base + offset;
     end = start + base + extra - 1;
-
-    if (end > y)
-        end = y;
+    if (end > y) end = y;
+    
 
     for (int i = start; i <= end; i++) {
-        if (isPrime(i)) local_count++;
+        if (isPrime(i)) {
+            local_primes[idx++] = i;
+            local_count++;
+        }
+    }
+
+    if (rank != 0) {
+        printf("p%d: calculate partial count of prime numbers from %d to %d  Count = %d (", rank, start, end, local_count);
+        for (int i = 0; i < idx; i++) {
+            printf("%d", local_primes[i]);
+            if (i < idx - 1) printf(", ");
+        }
+        printf(")\n");
     }
 
     MPI_Reduce(&local_count, &total_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    endTime = MPI_Wtime();
+    
+    if (rank != 0) {
+        MPI_Send(&idx, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(local_primes, idx, MPI_INT, 0, 1, MPI_COMM_WORLD);
+    }
 
     if (rank == 0) {
-        printf("Total prime numbers between %d and %d = %d\n", x, y, total_count);
+        int all_primes[1000], total_idx = 0;
+        for (int i = 0; i < idx; i++) {
+            all_primes[total_idx++] = local_primes[i];
+    }
+
+        
+        for (int i = 1; i < size; i++) {
+            int count;
+            MPI_Recv(&count, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if (count > 0) {
+                int buffer[100];
+                MPI_Recv(buffer, count, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                for (int j = 0; j < count; j++) {
+                    all_primes[total_idx++] = buffer[j];
+                }
+            }
+        }
+
+        endTime = MPI_Wtime();
+
+        printf("P0 will have Count = %d (", total_count);
+        for (int i = 0; i < total_idx; i++) {
+            printf("%d", all_primes[i]);
+            if (i < total_idx - 1) printf(", ");
+        }
+        printf(")\n");
+
         printf("Execution Time (MPI_Bcast + Reduce): %f seconds\n", endTime - startTime);
     }
 
